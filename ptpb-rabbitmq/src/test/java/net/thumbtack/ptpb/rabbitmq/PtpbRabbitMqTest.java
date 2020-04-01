87f7784ad869c5063438125e7e143a4ae7d023d8
Pilyaev Andrey <pitonchig@gmail.com>
2020-04-01 10:40:50 +0600

    All amqp entries renamed to rabbitmq

--- ptpb-rabbitmq/src/test/java/net/thumbtack/ptpb/rabbitmq/PtpbRabbitMqTest.java
+++ ptpb-rabbitmq/src/test/java/net/thumbtack/ptpb/rabbitmq/PtpbRabbitMqTest.java
@@ -1,10 +1,10 @@
-package net.thumbtack.ptpb.amqp;
+package net.thumbtack.ptpb.rabbitmq;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AmqpTest
+public class PtpbRabbitMqTest
 {
     @Test
     public void shouldAnswerWithTrue()
