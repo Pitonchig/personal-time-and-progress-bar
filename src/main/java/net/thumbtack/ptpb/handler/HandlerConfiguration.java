19897a48805556e45404a7e6d556a571c13bdde1
Pilyaev Andrey <pitonchig@gmail.com>
2020-07-26 23:08:59 +0600

    springfox-boot-starter used

--- src/main/java/net/thumbtack/ptpb/handler/HandlerConfiguration.java
+++ src/main/java/net/thumbtack/ptpb/handler/HandlerConfiguration.java
@@ -11,17 +11,17 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class HandlerConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
 
 
     @Bean
